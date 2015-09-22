fdc356f613cb7deff3ceb1254f418eff8e32b782
millsmgb <Matthew@Matthews-iMac.local>
2015-09-22 22:39:11 +1000

    Update to include repeating list

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var app = angular.module('flapperNews', []);
 
-//test
-
 app.controller('MainCtrl', [
 	'$scope',
 	function($scope){
 		$scope.test = 'Hello world!';
+		$scope.posts = [
+			'post 1',
+			'post 2',
+			'post 3',
+			'post 4',
+			'post 5'
+		];
 	}
 ]);
